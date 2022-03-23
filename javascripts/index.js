01a4dadec90b494cd046a32f3602df5a5a5f522d
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2022-03-23 16:39:14 -0400

    updated javascript to fetch api characters

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,4 +1,35 @@
 // this is for my javascript project
 document.addEventListener('DOMContentLoaded',()=>{
   console.log("Dom has loaded");
+  fetchAllCharacters()
 })
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
+function fetchAllCharacters(){
+  fetch('https://rickandmortyapi.com/api/character')
+  .then(response => response.json())
+  .then(characters => 
+    renderCharacters(characters.results))
+
+}
+
+function renderCharacters(characters){
+  characters.forEach(character => {
+    const div = document.createElement('div');
+    const cardUl = document.querySelector('.cards');
+    const image = document.createElement('img');
+    const name = document.createElement('h3');
+    const like = document.createElement('button');
+
+    div.classList = 'card'
+    image.src = character.image
+    name.innerText = character.name
+    like.innerHTML = EMPTY_HEART
+    like.classList = 'empty'
+    div.appendChild(image)
+    div.appendChild(name)
+    div.appendChild(like)
+    cardUl.appendChild(div)
+  
+  })
+}
