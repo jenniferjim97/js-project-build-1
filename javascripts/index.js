eb8582fb82854e721eff6c2fea582f01ae47835a
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2022-04-26 01:00:14 -0400

    cleaned code a bit

--- javascripts/index.js
+++ javascripts/index.js
@@ -6,10 +6,8 @@ document.addEventListener('DOMContentLoaded',()=>{
   backNextButton();
 })
 
-const EMPTY_HEART = '♡';
-const FULL_HEART = '❤️';
 const form = document.querySelector('form');
-const ulContainer = document.querySelector('.cards');
+const cardsContainer = document.querySelector('#cards-container');
 const allCharacters = document.getElementById('all-characters-link');
 const back = document.querySelector('.hidden-back')
 const next = document.querySelector('.hidden-next')
@@ -19,53 +17,53 @@ function fetchAllCharacters(){
   fetch('https://rickandmortyapi.com/api/character')
   .then(response => response.json())
   .then(characters => {
-    ulContainer.innerHTML = ' '
-    // console.log(characters)
+    cardsContainer.innerHTML = ' '
     renderCharacters(characters.results)
      back.classList.replace('hidden-back', 'show-back')
-    next.classList.replace('hidden-next', 'show-next')
+     next.classList.replace('hidden-next', 'show-next')
   })
 };
 
 function allCharacterslink(){
-  ulContainer.innerHTML = ''
+  cardsContainer.innerHTML = ''
   allCharacters.addEventListener('click', fetchAllCharacters);
 };
 
 function renderCharacters(characters){
   characters.forEach(character => {
     
-    const li = document.createElement('li');
-    const cardUl = document.querySelector('.cards');
+    const div = document.createElement('div');
     const image = document.createElement('img');
     const name = document.createElement('h3');
     const species = document.createElement('h3');
     const like = document.createElement('button');
     
-    li.classList = 'card'
+    div.classList = 'card'
     image.classList = 'card-img'
+    like.classList = 'empty'
+
     image.src = character.image
     name.innerText = `Name: ${character.name}`
     species.innerText =`Species: ${character.species}`
-    like.innerHTML = EMPTY_HEART
-    like.classList = 'empty'
-    li.appendChild(image)
-    li.appendChild(name)
-    li.appendChild(species)
-    li.appendChild(like)
-    cardUl.appendChild(li)
+    like.innerHTML = '♡'
+    
+    div.appendChild(image)
+    div.appendChild(name)
+    div.appendChild(species)
+    div.appendChild(like)
+    cardsContainer.appendChild(div)
 
     like.addEventListener('click', (e)=>{
-      if(like.innerHTML === EMPTY_HEART){
+      if(like.innerHTML === '♡'){
         like.classList.replace('empty', 'full')
-        like.innerHTML = FULL_HEART
+        like.innerHTML = '❤️'
       }
-      else if(like.innerHTML === FULL_HEART){
+      else if(like.innerHTML === '❤️'){
         like.classList.replace('full', 'empty')
-        like.innerHTML = EMPTY_HEART
+        like.innerHTML = '♡'
       }
-    })
-  })
+    });
+  });
 };
 
 
@@ -78,7 +76,7 @@ function searchCharacters(){
     fetch(`https://rickandmortyapi.com/api/character/?name=${e.target[0].value}`)
     .then(response => response.json())
     .then(data => {
-      ulContainer.innerHTML = ' '
+      cardsContainer.innerHTML = ' '
       renderCharacters(data.results)
 
       // console.log(data.results)
@@ -91,27 +89,26 @@ function searchCharacters(){
 
 function home(){
   const logo = document.querySelector('.logo');
-  logo.addEventListener('click', (e)=>{
-    ulContainer.innerHTML = ''
+    logo.addEventListener('click', (e)=>{
+    cardsContainer.innerHTML = ''
     back.classList.replace('show-back', 'hidden-back')
     next.classList.replace('show-next', 'hidden-next')
   })
 };
 
+
  function backNextButton(){
    let pageNum = 1
-   back.addEventListener('click', (e)=>{
+    back.addEventListener('click', (e)=>{
      console.log('ive been clicked')
      fetch(`https://rickandmortyapi.com/api/character?page=${--pageNum}`)
      .then(response=> response.json())
      .then(data => {console.log(data)
     // console.log(data.info.back)
-    ulContainer.innerHTML = ''
+    cardsContainer.innerHTML = ''
     renderCharacters(data.results)
     })
-
-
-   })
+   });
 
    next.addEventListener('click', (e)=>{
      console.log('ive also been clicked!')
@@ -119,8 +116,8 @@ function home(){
      .then(response=> response.json())
      .then(data => {console.log(data)
       console.log(data.info.next)
-      ulContainer.innerHTML= ''
+      cardsContainer.innerHTML= ''
       renderCharacters(data.results)
     })
    })
- }
+ };
